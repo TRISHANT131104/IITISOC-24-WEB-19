66c696b2474d9f0aec98d62b2b6162272070c37e
Abhinav072004 <158208515+Abhinav072004@users.noreply.github.com>
2024-06-19 21:43:20 +0530

    i-Chat app

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,29 @@ import 'dotenv/config'
 const app = express()
 const port = 4000
 
+import { Server } from "socket.io";
+
+const io = new Server(8000, { cors: { origin: "*" } });
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined',name=>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    })
+
+})
+
+
 app.use(express.json())
 app.use(cors())
 
