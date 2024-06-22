ed94b63310666fb21fd14020db0a4e54908229e8
Souravrai2005 <souravrai230105@gmail.com>
2024-06-22 14:24:57 +0530

    modified Home file

--- Frontend/frontend/src/components/Home.jsx
+++ Frontend/frontend/src/components/Home.jsx
@@ -12,3 +12,36 @@ const Home = () => {
 }
 
 export default Home
+<div class="container">
+        <div class="course">
+            <div class="preview">
+                <h6>Internship</h6>
+                <h2>Web Development</h2>
+                <a href="#">view all Internships</a>
+                <div class="info">
+                    <div class="progress-wrapper">
+                        <div class="progress">
+
+                        </div>
+                        <span class="progress-text">
+                            Experience -2yrs
+                        </span>
+                    </div>
+                    <h5>PART TIME</h6>
+                    
+                </div>
+            </div>
+            <div class="details">
+                <h2>FULL STACK DEVELOPMENT</h2>
+                <p class="p-trunc">SKILLS REQUIRED: HTML,CSS, JAVASCRIPT, MongoDB, Node.js, ReactJS, PHP <br>
+                    JOB TYPE : WORK FROM HOME<br>
+                    SALARY : ₹ 15,000/month<br>
+                    PERKS : CERTIFICATE , LETTER OF RECCOMENDATION.
+                </p>
+            
+            <button class="btn">
+                Apply Now ..
+            </button>
+            </div>
+        </div>
+    </div>
