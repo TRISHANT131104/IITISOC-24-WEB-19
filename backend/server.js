94a06254678eb109f3ed11319794e0ed221fb8a3
Abhinav072004 <158208515+Abhinav072004@users.noreply.github.com>
2024-06-28 00:24:24 +0530

    Readme file updated

--- backend/server.js
+++ backend/server.js
@@ -13,32 +13,14 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT || 4000
 
 
-// const io = new Server(4000, { cors: { origin: "*" } });
 
-// const users = {};
-
-// io.on('connection', socket => {
-//     socket.on('new-user-joined',name=>{
-//         users[socket.id] = name;
-//         socket.broadcast.emit('user-joined',name);
-//     });
-//     socket.on('send',message=>{
-//         socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
-//     });
-
-//     socket.on('disconnect', message =>{
-//         socket.broadcast.emit('left', users[socket.id]);
-//         delete users[socket.id];
-//     })
-
-// })
 
 
 app.use(express.json())
 app.use(cors())
 
 
-connectDB();
+// connectDB();
 
 
 app.use("/api/user", userRouter)
@@ -58,6 +40,6 @@ app.get('/',(req,res) => {
 )
 
 server.listen(PORT,()=>{
-    // connectDB();
+    connectDB();
     console.log(`Server started on http://localhost:${PORT}`)
 })
